04627c8101e8b0e24e0fa96f14af3a2577a5d906
Arnold <arnold201991@gmail.com>
2024-02-29 00:04:46 +0100

    añado nombre en llamada

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -28,8 +28,8 @@ const login = async (req, res) => {
             return res.status(401).json({ message: 'Contraseña incorrecta' });
         }
 
-        const token = jwt.sign({ userId: user._id, role: user.Roles }, process.env.JWT_SECRET, { expiresIn: '24h' });
-        res.status(200).json({ token, role: user.Roles });
+        const token = jwt.sign({ userId: user._id, role: user.Roles, nombre: user.username }, process.env.JWT_SECRET, { expiresIn: '24h' });
+        res.status(200).json({ token, role: user.Roles, nombre: user.username });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
