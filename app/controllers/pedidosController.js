110130e4e2130ff859faba2a5a85ef3520b9c820
Arnold <arnold201991@gmail.com>
2024-03-03 20:23:19 +0100

    se generan facturas

--- app/controllers/pedidosController.js
+++ app/controllers/pedidosController.js
@@ -1,4 +1,5 @@
 const Pedidos = require('../models/pedidosModel');
+const pdf = require('html-pdf');
 
 const getPedidos = async (req, res) => {
   try {
@@ -29,4 +30,36 @@ const postPedidos = async (req, res) => {
   }
 };
 
-module.exports = { getPedidos, postPedidos, getPedidosByUserId };
+const getFactura = async (req, res) => {
+  try {
+    const id = req.query.id;
+    const factura = await Pedidos.find({ id: id });
+    console.log("🚀 ~ getFactura ~ factura:", factura)
+    let productosHtmlElement = ""
+    if (factura[0]?.productos?.length) {
+      factura[0].productos.forEach(element => {
+        let elemento = `<p>${element.cantidad}</p>`
+        productosHtmlElement += elemento
+      })
+    } else {
+      let elemento = `<p>Sin productos</p>`
+      productosHtmlElement += elemento
+    }
+    let contenido = `
+      <h1>PCPiezas tu tienda de componentes</h1>
+      <p>${factura[0].totalImporte}</p>
+      <p>${factura[0].direccion}</p>
+      `;
+    contenido += productosHtmlElement
+    res.setHeader('Content-type', 'application/pdf');
+    pdf.create(contenido).toStream(function (err, stream) {
+      if (err) return res.send(err);
+      res.type('pdf');
+      stream.pipe(res);
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+};
+
+module.exports = { getPedidos, postPedidos, getPedidosByUserId, getFactura };
--- app/routes/pedidosRoutes.js
+++ app/routes/pedidosRoutes.js
@@ -1,4 +1,3 @@
-// pedidosRoutes.js
 const express = require('express');
 const router = express.Router();
 const pedidosController = require('../controllers/pedidosController'); 
@@ -6,5 +5,7 @@ const pedidosController = require('../controllers/pedidosController');
 router.get('/pedidos', pedidosController.getPedidos);
 router.post('/pedidos', pedidosController.postPedidos);
 router.get('/pedidosid', pedidosController.getPedidosByUserId);
+router.get('/factura', pedidosController.getFactura);
+
 
 module.exports = router;
