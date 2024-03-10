bd59250e9b009f1f65801f06d6bc24f28e81296a
Arnold <arnold201991@gmail.com>
2024-03-10 21:02:14 +0100

    mejora sistema tickets

--- app/controllers/ticketController.js
+++ app/controllers/ticketController.js
@@ -9,6 +9,26 @@ const getAllTickets = async (req, res) => {
   }
 };
 
+const getTicketsPorIdUsuario = async (req, res) => {
+  try {
+    const idUsuario = req.query.idUsuario;
+
+    if (!idUsuario) {
+      return res.status(400).json({ error: 'Parámetro idUsuario no proporcionado' });
+    }
+
+    const tickets = await Ticket.find({ idUsuario: { $regex: new RegExp(idUsuario, 'i') } });
+
+    if (!tickets || tickets.length === 0) {
+      return res.status(404).json({ error: 'No se encontraron tickets para el idUsuario proporcionado' });
+    }
+
+    return res.status(200).json({ tickets });
+  } catch (error) {
+    return res.status(500).json({ error: 'Error al obtener los tickets por idUsuario' });
+  }
+};
+
 const crearTicket = async (req, res) => {
   try {
     const { titulo, descripcion, idUsuario, fecha, estado } = req.body;
@@ -20,4 +40,33 @@ const crearTicket = async (req, res) => {
   }
 };
 
-module.exports = { getAllTickets,  crearTicket};
+const responderTicket = async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const { respuesta, estado } = req.body;
+
+    const ticketExistente = await Ticket.findById(_id);
+    if (!ticketExistente) {
+      return res.status(404).json({ error: 'Ticket no encontrado' });
+    }
+
+    if (estado) {
+      // Actualiza el estado solo si se proporciona un nuevo estado
+      ticketExistente.estado = estado;
+    }
+
+    if (respuesta !== undefined) {
+      // Actualiza la respuesta solo si se proporciona una nueva respuesta
+      ticketExistente.respuesta = respuesta;
+    }
+
+    await ticketExistente.save();
+
+    return res.json(ticketExistente);
+  } catch (error) {
+    console.error('Error al responder al ticket:', error);
+    res.status(500).json({ error: 'Error al responder al ticket' });
+  }
+};
+
+module.exports = { getAllTickets,  crearTicket, getTicketsPorIdUsuario, responderTicket };
--- app/routes/ticketRoutes.js
+++ app/routes/ticketRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const ticketController = require('../controllers/ticketController');
 
 router.get('/recibirTicket', ticketController.getAllTickets);
+router.get('/recibirTicketId', ticketController.getTicketsPorIdUsuario);
 router.post('/nuevoTicket', ticketController.crearTicket);
+router.post('/responderTicket/:_id', ticketController.responderTicket);
 
 module.exports = router;
