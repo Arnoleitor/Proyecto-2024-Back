2b7080bb91c0a141132d09db8871dc149b6ec926
Arnold <arnold201991@gmail.com>
2024-03-13 22:41:49 +0100

    nueva prop

--- app/models/codigosDescuentoModel.js
+++ app/models/codigosDescuentoModel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const codigosDescuentoSchema = new mongoose.Schema({
-  codigo: String
+  codigo: String,
+  descuento: Number
 });
 
 const CodigosDescuento = mongoose.model('codigosdescuentos', codigosDescuentoSchema);
